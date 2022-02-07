e2a184797e6a8e2d71eabb000e2bb896fdd35582
mercy.waeni <mercy.waeni@calltronix.com>
2022-02-07 11:57:52 +0300

    complete getters and setters lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Add your Circle class here
+class Circle{
+    constructor(radius){
+        this.radius = radius;
+    }
+
+    get diameter(){
+        return this.radius*2
+    }
+
+    get area(){
+        return Math.PI*(this.radius*this.radius)
+    }
+
+    get circumference(){
+        return 2*Math.PI*this.radius
+    }
+
+    set diameter(newDiameter){
+        this.radius = newDiameter/2
+    }
+
+    set area(newArea){
+        this.radius = Math.sqrt(newArea/Math.PI)
+    }
+
+    set circumference(newCircumfrence){
+        this.radius = newCircumfrence/(2*Math.PI)
+    }
+}
